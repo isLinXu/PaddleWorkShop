31ebebb841ba2c148983c6d752b3751a0e9733f1
Xu Lin <2267379130@qq.com>
2022-02-17 23:41:02 +0800

    1.update model.py

--- mobilenet/model.py
+++ mobilenet/model.py
@@ -14,3 +14,19 @@ def conv_bn_layer(input, filter_size, num_filters, stride,
                                bias_attr=False)
 
     return fluid.layers.batch_norm(input=conv, act=act)
+
+def depthwise_separable(input, num_filters1, num_filters2, num_groups, stride, scale):
+    depthwise_conv = conv_bn_layer(input=input,
+                                   filter_size=3,
+                                   num_filters=int(num_filters1 * scale),
+                                   stride=stride,
+                                   padding=1,
+                                   num_groups=int(num_groups * scale),
+                                   use_cudnn=False)
+
+    pointwise_conv = conv_bn_layer(input=depthwise_conv,
+                                   filter_size=1,
+                                   num_filters=int(num_filters2 * scale),
+                                   stride=1,
+                                   padding=0)
+    return pointwise_conv
