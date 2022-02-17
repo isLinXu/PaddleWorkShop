93ce400f77bb4bc34ee0788466f48d15b767dc3f
Xu Lin <2267379130@qq.com>
2022-02-17 23:50:17 +0800

    1.update train.py

--- mobilenet/train.py
+++ mobilenet/train.py
@@ -39,3 +39,52 @@ train_reader = paddle.batch(reader=reader.train_reader('images/train.list', crop
 test_reader = paddle.batch(reader=reader.test_reader('images/test.list', crop_size), batch_size=32)
 
 
+# 定义一个使用GPU的执行器
+place = fluid.CUDAPlace(0)
+# place = fluid.CPUPlace()
+exe = fluid.Executor(place)
+# 进行参数初始化
+exe.run(fluid.default_startup_program())
+
+# 定义输入数据维度
+feeder = fluid.DataFeeder(place=place, feed_list=[image, label])
+
+
+# 训练100次
+for pass_id in range(100):
+    # 进行训练
+    for batch_id, data in enumerate(train_reader()):
+        train_cost, train_acc = exe.run(program=fluid.default_main_program(),
+                                        feed=feeder.feed(data),
+                                        fetch_list=[avg_cost, acc])
+
+        # 每100个batch打印一次信息
+        if batch_id % 100 == 0:
+            print('Pass:%d, Batch:%d, Cost:%0.5f, Accuracy:%0.5f' %
+                  (pass_id, batch_id, train_cost[0], train_acc[0]))
+
+    # 进行测试
+    test_accs = []
+    test_costs = []
+    for batch_id, data in enumerate(test_reader()):
+        test_cost, test_acc = exe.run(program=test_program,
+                                      feed=feeder.feed(data),
+                                      fetch_list=[avg_cost, acc])
+        test_accs.append(test_acc[0])
+        test_costs.append(test_cost[0])
+    # 求测试结果的平均值
+    test_cost = (sum(test_costs) / len(test_costs))
+    test_acc = (sum(test_accs) / len(test_accs))
+    print('Test:%d, Cost:%0.5f, Accuracy:%0.5f' % (pass_id, test_cost, test_acc))
+
+
+    # 保存预测模型
+    save_path = 'infer_model/'
+    # 删除旧的模型文件
+    shutil.rmtree(save_path, ignore_errors=True)
+    # 创建保持模型文件目录
+    os.makedirs(save_path)
+    # 保存预测模型
+    fluid.io.save_inference_model(save_path, feeded_var_names=[image.name], target_vars=[model], executor=exe)
+
+
